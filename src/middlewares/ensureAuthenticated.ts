484f602314ec62630dd8b80f76d242f7652e3118
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2025-01-25 18:50:02 -0300

    Extracting the Token

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,19 @@
+import { AppError } from "@/utils/AppError";
+import { Request, Response, NextFunction } from "express"
+
+function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+    const authHeader = request.headers.authorization
+
+    if(!authHeader){
+        throw new AppError("JWT Token not specified.", 401);
+        
+    }
+
+    const [,token] = authHeader.split(" ")
+
+    console.log(token)
+
+    return next()
+}
+
+export { ensureAuthenticated }
