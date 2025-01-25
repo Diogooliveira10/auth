5163e5d3104cefa8b2aa0b2bfec067167bd56e05
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2025-01-25 19:02:33 -0300

    Authentication Middleware

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,17 +1,22 @@
 import { AppError } from "@/utils/AppError";
 import { Request, Response, NextFunction } from "express"
+import { verify } from "jsonwebtoken"
+import { authConfig } from "@/configs/auth"
 
 function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
     const authHeader = request.headers.authorization
 
     if(!authHeader){
         throw new AppError("JWT Token not specified.", 401);
-        
     }
 
     const [,token] = authHeader.split(" ")
 
-    console.log(token)
+    const { sub: user_id } = verify(token, authConfig.jwt.secret)
+
+    request.user = {
+        id: String(user_id)
+    }
 
     return next()
 }
