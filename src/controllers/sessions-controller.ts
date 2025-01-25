eee8d6db3c38413d68eaa3307c14398933f7ded6
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2025-01-25 18:40:20 -0300

    Creating Session

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,29 @@
 import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError"
+import { authConfig } from "@/configs/auth"
+import { sign } from "jsonwebtoken"
 
 class SessionsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "ok" })
+    const { username, password } = request.body
+
+    const fakeUser = {
+      id: 1,
+      username: "diogo",
+      password: "123456",
+    }
+
+    if(username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError("Incorrect username and/or password!", 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      expiresIn,
+      subject: String(fakeUser.id)
+    })
+
+    return response.json({ token })
   }
 }
 
